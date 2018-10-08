b9b980bb47b331f1ff3e7064f5b94d15e124ad94
Roland4444 <turbopollux@rambler.ru>
2018-10-08 11:01:33 +0000

    full files tree

---
+++ wrapperToSummPointers/wrappedToPointers/main.c
@@ -0,0 +1,29 @@
+// The functions contained in this file are pretty dummy
+// and are included only as a placeholder. Nevertheless,
+// they *will* get included in the shared library if you
+// don't remove them :)
+//
+// Obviously, you 'll have to write yourself the super-duper
+// functions to include in the resulting library...
+// Also, it's not necessary to write every function in this file.
+// Feel free to add more files in this project. They will be
+// included in the resulting library.
+
+// A function adding two integers and returning the result
+#include <dlfcn.h>
+typedef int (*structSummPointer)(struct input* inp);
+int wstructSummPointer(struct input* inp){
+    void* handle = dlopen("./libsummwPointers.so", RTLD_LAZY);
+    if (!handle){
+        printf("\nerror opened\n");
+        return -2;
+    }
+    printf("\nsucces open so\n");
+    structSummPointer load = (structSummPointer)(dlsym(handle, "SummwPointers"));
+    if (!load)
+    {
+        printf("\nerror opened symbol\n");
+        return -3;
+    }
+    return load(inp);
+}
---
+++ wrappertoSimpleSumm/wSimpleSumm/main.c
@@ -0,0 +1,30 @@
+// The functions contained in this file are pretty dummy
+// and are included only as a placeholder. Nevertheless,
+// they *will* get included in the shared library if you
+// don't remove them :)
+//
+// Obviously, you 'll have to write yourself the super-duper
+// functions to include in the resulting library...
+// Also, it's not necessary to write every function in this file.
+// Feel free to add more files in this project. They will be
+// included in the resulting library.
+
+// A function adding two integers and returning the result
+#include <dlfcn.h>
+typedef int (*SimpleSumm)( int , int);
+int wSimpleSumm(int a, int b){
+    void* handle = dlopen("./libsimpleSumm.so", RTLD_NOW);
+    if (!handle){
+        printf("\nerror opened\n");
+        return -2;
+    }
+    printf("\nsucces open so\n");
+    SimpleSumm load = (SimpleSumm)(dlsym(handle, "simpleSumm"));
+    if (!load)
+    {
+        printf("\nerror opened symbol\n");
+        return -3;
+    }
+
+    return load(a,b);
+};
